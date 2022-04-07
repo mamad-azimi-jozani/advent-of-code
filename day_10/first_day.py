529f0444714c4e3ad13682aa451b1b8e2bbb9c72
mamad-azimi-jozani <azimi.jozani.mohammad@gmail.com>
2022-04-07 09:56:46 +0430

    executed oop and there is nothing to say :)

--- day_10/first_day.py
+++ day_10/first_day.py
@@ -30,3 +30,37 @@ score = {
 }
 
 print(sum(list(map(lambda i: score[i], illegal))))
+
+
+
+### oop ###
+class SyntaxScore:
+    def __init__(self, data):
+        self.data = data
+
+    # string = '{<[[]]>}<{[{[{[]{()[[[]'
+    def calculate(self):
+        stack = []
+        close = ['>', ')', '}', ']']
+        open = ['[', '<', '{', '(']
+        pair = ['<>', '()', '{}', "[]"]
+        illegal = []
+        score = {
+            ')': 3,
+            ']': 57,
+            '}': 1197,
+            '>': 25137,
+        }
+        for j in self.data:
+            for i in j:
+                if i in open:
+                    stack.append(i)
+                if i in close:
+                    if stack[-1] + i in pair:
+                        # print(stack[-1] + i)
+                        stack.pop()
+                    else:
+                        illegal.append(i)
+                        stack.pop()
+
+        return sum(list(map(lambda i: score[i], illegal)))
